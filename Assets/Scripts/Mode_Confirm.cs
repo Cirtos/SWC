1e67c9102c2f914c09ed5926e75b004bca7e5cfa
Batman <shane.byrne@pulsecollegestudent.com>
2016-07-29 17:30:20 +0300

    emu papers, menu scripts added

---
+++ Assets/Scripts/Mode_Confirm.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mode_Confirm : MonoBehaviour
+{
+    public string levelToLoad;
+
+    private bool focus;
+    private string colTag;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (focus == true)
+        {
+            if (colTag == "Russia")
+            {
+                if (Input.GetButtonDown("P2 Fire"))
+                {
+                    if (levelToLoad != "")
+                        Application.LoadLevel(levelToLoad);
+                }
+
+            }
+            else if (colTag == "America")
+            {
+                if (Input.GetButtonDown("P1 Fire"))
+                {
+                    if (levelToLoad != "")
+                        Application.LoadLevel(levelToLoad);
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        focus = true;
+        colTag = col.gameObject.tag;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        focus = false;
+        colTag = "";
+    }
+}
---
+++ Assets/Scripts/Mode_Select.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mode_Select : MonoBehaviour {
+    
+    public string levelToLoad;
+    public Sprite button;
+    public Sprite buttonPressed;
+
+    private SpriteRenderer sprite;
+    private Mode_Confirm modeConfirm;
+    private bool selected;
+    private Mode_Select[] modeButtons;
+    private bool focus;
+    private string colTag;
+
+    // Use this for initialization
+    void Start () {
+        modeButtons = FindObjectsOfType<Mode_Select>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected)
+            sprite.sprite = buttonPressed;
+        else
+            sprite.sprite = button;
+
+        if (focus == true)
+        {
+            if (colTag == "Russia")
+            {
+                if (Input.GetButtonDown("P2 Fire"))
+                {
+                    foreach (Mode_Select modeButton in modeButtons)
+                    {
+                        modeButton.selected = false;
+                    }
+
+                    selected = true;
+                    modeConfirm.levelToLoad = levelToLoad;
+                }
+            }
+
+            else if (colTag == "America")
+            {
+                if (Input.GetButtonDown("P1 Fire"))
+                {
+                    foreach (Mode_Select modeButton in modeButtons)
+                    {
+                        modeButton.selected = false;
+                    }
+
+                    selected = true;
+                    modeConfirm.levelToLoad = levelToLoad;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        focus = true;
+        colTag = col.gameObject.tag;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        focus = false;
+        colTag = "";
+    }
+}
